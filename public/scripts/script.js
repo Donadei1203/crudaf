89aa7a3b6304a5393e7f7357bc34f96beb71bc44
Donadei1203 <gu1203.doman@gmail.com>
2025-12-04 08:07:19 -0300

    Add files via upload

---
+++ public/scripts/script.js
@@ -0,0 +1,32 @@
+function btocadastrar(event) {
+  event.preventDefault();
+
+  var usuario = {
+    nome: document.getElementById("nome").value,
+    senha: document.getElementById("senha").value,
+    cpf: document.getElementById("cpf").value,
+    email: document.getElementById("email").value,
+    cep:document.getElementById('cep').value,
+    cidade: document.getElementById("cidade").value,
+    estado: document.getElementById("estado").value,
+    rua: document.getElementById("rua").value,
+    telefone: document.getElementById("telefone").value
+  };
+
+  fetch("http://localhost:3000/usuarios", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(usuario),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert("Usuário cadastrado!");
+      console.log("Sucesso:", data);
+    })
+    .catch((err) => {
+      alert("Erro ao cadastrar usuário.");
+      console.error("Erro:", err);
+    });
+}
