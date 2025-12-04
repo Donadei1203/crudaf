5733a8af10e8798eafcf0b08e59a0c39a06a54ee
Donadei1203 <gu1203.doman@gmail.com>
2025-12-04 08:35:36 -0300

    Add files via upload

---
+++ public/scripts/profile.js
@@ -0,0 +1,34 @@
+function check(){
+    var loginls = localStorage.getItem('loginLS')
+    var senhals = localStorage.getItem('senhaLS')
+
+
+    
+    console.log(loginls + senhals)
+    
+    fetch('https://crudaf-1.onrender.com/usuarios').then(resposta => resposta.json()).then(data => {
+        const pessoa = data.find(p => p.nome == loginls)
+        
+        if (pessoa) {
+        document.getElementById('nome').value = pessoa.nome;
+        document.getElementById('senha').value = pessoa.senha;
+        document.getElementById('cpf').value = pessoa.cpf;
+        document.getElementById('email').value = pessoa.email;
+        document.getElementById('cep').value = pessoa.cep;
+        document.getElementById('cidade').value = pessoa.cidade;
+        document.getElementById('estado').value = pessoa.estado;
+        document.getElementById('rua').value = pessoa.rua;
+        document.getElementById('telefone').value = pessoa.telefone;
+    } else{
+        console.log('Não logado')
+    }
+    })
+}
+
+window.onload = check()
+
+function logout(){
+  localStorage.removeItem('loginLS');
+  localStorage.removeItem('senhaLS');
+  window.location.href = '../index.html'
+}
